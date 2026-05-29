783d1da65203cc9b493b9b1fcb786c5044ff44de
jayanderson-1 <jayandersonsouza@gmail.com>
2026-05-29 15:56:25 -0300

    Create RootLayout and Home components in new files

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
+
+clica em commit changes
+
+criar o arquivo app/page.tsx
+clica em add arquivo>criar novo arquivo 
+
+nome digita app/page.tsx
+
+e cola esse agr 
+
+export default function Home() {
+  return <div>Conquista Já</div>
+}

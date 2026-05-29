487f13a82f8fbadf3c54532ab8fdddb0f40abb0a
jayanderson-1 <jayandersonsouza@gmail.com>
2026-05-29 18:25:34 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,18 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Conquista Já',
+  description: 'Conquista Já',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  )
-}
-
-clica em commit changes
-
-criar o arquivo app/page.tsx
-clica em add arquivo>criar novo arquivo 
-
-nome digita app/page.tsx
-
-e cola esse agr 
-
-export default function Home() {
-  return <div>Conquista Já</div>
+  );
 }
